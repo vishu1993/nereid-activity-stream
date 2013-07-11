36fd31011fc7040fa72ddd3e429cc847fcc14884
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2013-07-11 17:21:21 +0530

    PEPification and Updated .travis.yml #1525
    Cleaned code as per flake8 and updated travis.yml
    
    Review ID: 240003

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-    init
+    test init
 
     :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
     :license: GPLv3, see LICENSE for more details.
 """
-from test_activity_stream import suite
+import unittest
+import trytond.tests.test_tryton
+
+from test_activity_stream import ActivityTestCase
+
+
+def suite():
+    """
+    Define suite
+    """
+    test_suite = trytond.tests.test_tryton.suite()
+    test_suite.addTests([
+        unittest.TestLoader().loadTestsFromTestCase(ActivityTestCase),
+    ])
+    return test_suite
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(verbosity=2).run(suite())
